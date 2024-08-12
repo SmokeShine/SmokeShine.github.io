baa5832c56035fd5d00684c55bb2b4f06b116b02
Prateek Gupta <prateekgupta.nitjsr@gmail.com>
2024-08-12 23:58:31 +0530

    Added index.html

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript to enhance interactivity
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+});
